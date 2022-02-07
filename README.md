12af118b7f1a9ec46be3553f1d5dbfadec1c6c57
Polo <poloaguirre3421@gmail.com>
2022-02-06 20:18:58 -0700

    Initial Commit

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# week3
+# HW1
 
-[![Run in Postman](https://run.pstmn.io/button.svg)](https://app.getpostman.com/run-collection/1e37a1a45fd828a9cb10)
+[![Run in Postman](https://run.pstmn.io/button.svg)](https://god.postman.co/run-collection/65359efeac42ac08e245?action=collection%2Fimport#?env%5BHW1%5D=W3sia2V5IjoiJGVjaG9fYm9keSIsInZhbHVlIjoidGVzdCB2YWx1ZSIsImVuYWJsZWQiOnRydWUsInR5cGUiOiJhbnkiLCJzZXNzaW9uVmFsdWUiOiJ5dXAiLCJzZXNzaW9uSW5kZXgiOjB9XQ==)
--- test/echotest.js
+++ test/echotest.js
@@ -1,27 +1,30 @@
 //Require the dev-dependencies
-let chai = require('chai');
-let chaiHttp = require('chai-http');
-let server = require('../app');
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+const { reset } = require("sinon");
+let server = require("../app");
 let should = chai.should();
 
 chai.use(chaiHttp);
 
-describe('Echo', () => {
-    beforeEach((done) => { //Before each
-            done();
-    });
+describe("Echo", () => {
+  beforeEach((done) => {
+    //Before each
+    done();
+  });
 
-    describe('/', () => {
-        it('it should POST echo', (done) => {
-            chai.request(server)
-                .post('/')
-                .send('hello world')
-                .end((err, res) => {
-                    res.should.have.status(200);
-                    res.text.should.eq('hello world');
-                    done();
-                });
+  describe("/", () => {
+    it("it should POST echo", (done) => {
+      chai
+        .request(server)
+        .post("/")
+        .send("hello world")
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.resp;
+          res.text.should.eq("hello world");
+          done();
         });
     });
-
+  });
 });
